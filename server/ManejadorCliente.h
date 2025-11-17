4281df2a60ffcc79bcc0d2ae52950b4605c51b4c
dmolloc1 <dmolloc@unsa.edu.pe>
2025-11-17 16:55:37 -0500

    Implementacion de uso qt

--- server/ManejadorCliente.h
+++ server/ManejadorCliente.h
@@ -1,26 +1,43 @@
 #ifndef MANEJADORCLIENTE_H
 #define MANEJADORCLIENTE_H
 
-#include <string>
-#include <vector>
+#include <QObject>
+#include <QTcpSocket>
+#include <QString>
+#include "common/models/Estados.h"
 
-class ManejadorCliente {
-
+class ManejadorCliente : public QObject {
+  Q_OBJECT
 public:
-    explicit ManejadorCliente(int socketDescriptor);
-    virtual ~ManejadorCliente();
+  explicit ManejadorCliente(qintptr socketDescriptor, QObject* parent = nullptr);
+  virtual ~ManejadorCliente();
 
-    void procesar();
-    void enviarMensaje(const std::string& mensaje);
+  TipoActor getTipoActor() const;
+  int getIdActor() const;
+  QString getNombreEstacion() const;
+
+public slots:
+  void procesar();
+  void enviarMensaje(const QJsonObject& mensaje);
+
+private slots:
+  void listoParaLeer();
+  void desconectado();
+
+signals:
+  void finished();
 
 private:
-    void leerDatos();
-    void procesarBuffer();
-    void desconectado();
+  void procesarBuffer();
+  void identificarCliente(const QJsonObject& data);
 
-private:
-    int m_socketDescriptor;
-    std::string m_buffer;
+  qintptr m_socketDescriptor;
+  QTcpSocket* m_socket = nullptr;
+  QByteArray m_buffer;
+
+  TipoActor m_tipoActor = TipoActor::DESCONOCIDO;
+  int m_idActor = -1;
+  QString m_nombreEstacion;
 };
 
 #endif
