667ed12c43c8f8f692ca1a1ae82fe11c653a067f
dmolloc1 <dmolloc@unsa.edu.pe>
2025-11-17 16:43:25 -0500

    Receive the data stream and proccess it

---
+++ server/ManejadorCliente.h
@@ -0,0 +1,31 @@
+#ifndef MANEJADORCLIENTE_H
+#define MANEJADORCLIENTE_H
+#include <QObject>
+#include <QTcpSocket>
+
+class ManejadorCliente : public QObject {
+  Q_OBJECT
+public:
+  explicit ManejadorCliente(qintptr socketDescriptor, QObject* parent = nullptr);
+  virtual ~ManejadorCliente();
+
+public slots:
+  void procesar();
+  void enviarMensaje(const QJsonObject& mensaje);
+
+private slots:
+  void listoParaLeer();
+  void desconectado();
+
+signals:
+  void finished();
+
+private:
+  void procesarBuffer();
+
+  qintptr m_socketDescriptor;
+  QTcpSocket* m_socket = nullptr;
+  QByteArray m_buffer;
+};
+
+#endif
