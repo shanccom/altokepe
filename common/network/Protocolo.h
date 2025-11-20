d55fe6c53e04d9d4978dad8831505f4265e1cee1
AjraHuacsoJeansAnthony <jajra@unsa.edu.pe>
2025-11-20 00:33:51 -0500

    Se agregan claves generales para mensajes cliente-servidor

---
+++ common/network/Protocolo.h
@@ -0,0 +1,12 @@
+#ifndef PROTOCOLO_H
+#define PROTOCOLO_H
+
+namespace Protocolo {
+  // --- Claves Generales ---
+  constexpr const char* COMANDO = "comando";
+  constexpr const char* EVENTO = "evento";
+  constexpr const char* DATA = "data";
+
+  }
+  
+#endif
