567e7478b1af9ae5f6f656a6d5de201681f86dc5
Choflis <lluquecon@unsa.edu.pe>
2025-11-25 22:51:06 -0500

    Implementamos los headers de UI para VentanaEstacion y VentanaUnificada

---
+++ clients/cocina/manager_estacion/ui/VentanaEstacion.h
@@ -0,0 +1,38 @@
+
+#pragma once
+
+#include <QWidget>
+#include <QTableWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QString>
+
+class VentanaEstacion : public QWidget {
+    Q_OBJECT
+public:
+    struct InfoPlatoVisual {
+        QString nombrePlato;
+        double prioridad;
+        QString estado;
+        long long id_pedido;
+        long long id_instancia;
+    };
+
+    explicit VentanaEstacion(const QString& nombreEstacion, QWidget* parent = nullptr);
+
+    void cargarPlatosIniciales(const std::vector<InfoPlatoVisual>& platos);
+    void agregarNuevoPlato(const InfoPlatoVisual& plato);
+    void actualizarEstadoPlato(long long idPedido, long long idInstancia, const QString& nuevoEstado);
+
+signals:
+    void marcarListoSolicitado(long long idPedido, long long idInstancia);
+
+private slots:
+    void onFinalizarMayorPrioridad();
+
+private:
+    QTableWidget* tabla;
+    QPushButton* btnFinalizar;
+    QString m_estacion;
+    std::vector<InfoPlatoVisual> platosOrdenados;
+};
