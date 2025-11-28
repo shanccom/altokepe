165a100d70d5a42442ba969ddef0df150189d18d
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-11-28 17:10:53 -0500

    fix(server): agrega nuevos enums que usa server para manipular los datos

--- common/models/InfoPlatoPrioridad.h
+++ common/models/InfoPlatoPrioridad.h
@@ -8,6 +8,11 @@ struct InfoPlatoPrioridad {
   long long id_instancia_plato;
   double score_prioridad; 
 
+  InfoPlatoPrioridad() : id_pedido(0), id_instancia_plato(0), score_prioridad(0.0) {}
+
+  InfoPlatoPrioridad(long long idInstancia, double score)
+    : id_pedido(0), id_instancia_plato(idInstancia), score_prioridad(score) {}
+
   // Comparador para que std::priority_queue se comporte como un MinHeap.
   // Se define aquí, pero usaremos std::greater en la declaración de la cola.
   bool operator>(const InfoPlatoPrioridad& other) const {
